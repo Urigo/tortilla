b4b2515ec7bdd3fb0414ae18f1b944830c318ced
DAB0mB <emanor6@gmail.com>
2016-10-10 13:59:55 -0200

    Update README.md to have instructions about manual files and how to use them

--- tests/test.js
+++ tests/test.js
@@ -41,7 +41,7 @@ beforeEach(function () {
   ].concat(argv), ...args);
 
   this.npm.manual = (argv, ...args) => this.npm([
-    'run', 'manual', '--'
+    'run', 'manuals', '--'
   ].concat(argv), ...args);
 
   this.git.apply = (patchName) => {
