51c52bd098536374988ccceba0550856ce091900
dotansimha <dotansimha@gmail.com>
2018-04-22 19:20:43 +0300

    try to fix ci

--- src/utils.js
+++ src/utils.js
@@ -30,11 +30,11 @@ let node;
 
   // Setting all relative utils
   exec.print = spawn;
-  git = (process.env.CI ? spawn : exec).bind(null, 'git');
+  git = exec.bind(null, 'git');
   git.print = spawn.bind(null, 'git');
-  npm = process.env.CI ? spawn : exec.bind(null, 'npm');
+  npm = exec.bind(null, 'npm');
   npm.print = spawn.bind(null, 'npm');
-  node = process.env.CI ? spawn : exec.bind(null, 'node');
+  node = exec.bind(null, 'node');
   node.print = spawn.bind(null, 'node');
   // It's better to have a getter rather than an explicit value otherwise
   // it might be reset
