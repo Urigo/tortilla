1fbe5e9968de56594054525fd707f199950f5f48
DAB0mB <emanor6@gmail.com>
2018-06-12 09:23:52 +0800

    Get rid of the binary flag when comparing 2 releases

--- src/release.ts
+++ src/release.ts
@@ -292,10 +292,10 @@ function diffRelease(
   let result
   if (sourceReleaseTag) {
     // Run 'diff' between the newly created commits
-    result = Git.print(['diff', '--binary', 'HEAD^', 'HEAD'].concat(argv), gitOptions);
+    result = Git.print(['diff', 'HEAD^', 'HEAD'].concat(argv), gitOptions);
   } else {
     // Run so called 'diff' between HEAD and --root. A normal diff won't work here
-    result = Git.print(['show', '--binary', '--format='].concat(argv), gitOptions);
+    result = Git.print(['show', '--format='].concat(argv), gitOptions);
   }
 
   // Clear registers
